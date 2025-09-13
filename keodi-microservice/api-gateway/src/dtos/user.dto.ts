db7a69ee15dacd59508d4458362a9d3df8a90d31
Tran-Trung-Nhut <ttnhut904@gmail.com>
2025-09-14 00:00:25 +0700

    add + ^ to special character

--- keodi-microservice/api-gateway/src/dtos/user.dto.ts
+++ keodi-microservice/api-gateway/src/dtos/user.dto.ts
@@ -12,7 +12,7 @@ export class CreateUserDto {
     @MinLength(8, { message: 'Mật khẩu có ít nhất 8 ký tự' })
     @Matches(/.*[A-Z].*/, { message: 'Mật khẩu phải chứa ít nhất 1 chữ hoa' })
     @Matches(/.*\d.*/, { message: 'Mật khẩu phải chứa ít nhất 1 số' })
-    @Matches(/.*[@$!%*?&].*/, { message: 'Mật khẩu phải chứa ít nhất 1 ký tự đặc biệt' })
+    @Matches(/.*[@$!%*?&^+].*/, { message: 'Mật khẩu phải chứa ít nhất 1 ký tự đặc biệt' })
     @ApiProperty({ example: 'Abc123!@#', description: 'Mật khẩu (tối thiểu 8 ký tự, gồm ít nhất 1 chữ hoa, 1 số, 1 ký tự đặc biệt)' })
     password: string
 
