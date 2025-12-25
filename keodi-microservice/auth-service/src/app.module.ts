f7dd4b2df7fb49b39400f0779ae312370ba7963b
nhut.trantrung <nhut.trantrung@nexondv.com>
2025-12-25 13:57:55 +0700

    chore: replace dotenv by using nest config

--- keodi-microservice/auth-service/src/app.module.ts
+++ keodi-microservice/auth-service/src/app.module.ts
@@ -3,10 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    ConfigModule.forRoot({ isGlobal: true })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
