0aa7a787e9d070ed2538f603a5b7e0e0f614d175
nhut.trantrung <nhut.trantrung@nexondv.com>
2026-01-20 15:59:48 +0700

    refactor: create configs folder to make code cleaner

--- keodi-microservice/auth-service/src/configs/prisma.config.ts
+++ keodi-microservice/auth-service/src/configs/prisma.config.ts
@@ -2,9 +2,9 @@ import { defineConfig, env } from "prisma/config";
 import 'dotenv/config';
 
 export default defineConfig({
-    schema: 'src/database/prisma/schema.prisma',
+    schema: '../database/prisma/schema.prisma',
     migrations: {
-        path: 'src/database/prisma/migrations',
+        path: '../database/prisma/migrations',
     },
     datasource: {
         url: env('DATABASE_URL')
