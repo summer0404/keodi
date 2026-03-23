1b51180a07af2c54980d40293c7c48d8d366d282
summer0404 <157554348+summer0404@users.noreply.github.com>
2026-03-23 20:09:13 +0700

    Add categories and opening hours to the new get place trending

--- keodi-microservice/core-service/src/shared/constants/recommendation.constant.ts
+++ keodi-microservice/core-service/src/shared/constants/recommendation.constant.ts
@@ -1,7 +1,7 @@
 export const RecommendationRedisKeys = {
   PLACES_FROM_SEARCH_TERMS: 'place:search:trending',
   PLACES_FROM_USER_ACTIONS: 'place:user-action:trending',
-}
+};
 
 export const PLACES_PER_SEARCH_TERM = 2;
 
