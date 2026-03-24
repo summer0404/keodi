6be3d0dfa2ee3f20d6e983e1f7d2f39a175b07d4
nhut.trantrung <nhut.trantrung@nexondv.com>
2026-03-24 16:34:51 +0700

    fix: missing Prisma DeviceToken and NotificationInbox in app module in Notification service

--- keodi-microservice/notification-service/src/app.module.ts
+++ keodi-microservice/notification-service/src/app.module.ts
@@ -2,12 +2,18 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { NotificationModule } from './modules/notification/notification.module';
 import { ProviderModule } from './providers/provider.module';
+import { PrismaModule } from './database/prisma.module';
+import { NotificationInboxModule } from './modules/notification-inbox/notification-inbox.module';
+import { DeviceTokenModule } from './modules/device-token/device-token.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     ProviderModule,
     NotificationModule,
+    NotificationInboxModule,
+    DeviceTokenModule,
+    PrismaModule
   ],
 })
 export class AppModule {}
