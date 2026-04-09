b72697343e9dd7f157b72867b0373c1c2e4c2ba6
nhut.trantrung <nhut.trantrung@nexondv.com>
2026-04-09 11:41:42 +0700

    fix: prisma.config cannot resolt database url

--- keodi-microservice/notification-service/src/configs/prisma.config.ts
+++ keodi-microservice/notification-service/src/configs/prisma.config.ts
@@ -1,5 +1,5 @@
+import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
-import 'dotenv/config';
 
 export default defineConfig({
     schema: '../database/prisma/schema.prisma',
@@ -7,6 +7,6 @@ export default defineConfig({
         path: '../database/prisma/migrations',
     },
     datasource: {
-        url: env('DATABASE_URL')
+        url: process.env.DATABASE_URL!,
     }
 })
