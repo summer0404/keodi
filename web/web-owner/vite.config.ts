5c5f30255b85eeb75142259d4e363c14c33c6fe7
summer0404 <157554348+summer0404@users.noreply.github.com>
2026-04-23 20:09:05 +0700

    fix: fix config files

--- web/web-owner/vite.config.ts
+++ web/web-owner/vite.config.ts
@@ -1,6 +1,6 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
