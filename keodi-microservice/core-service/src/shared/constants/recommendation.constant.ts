9cdc574b8e905bacfd53a4cc25382fc7e358bf25
Võ Xuân Hạ <157554348+summer0404@users.noreply.github.com>
2026-05-04 22:07:14 +0700

    fix: change min from 3 to 2 (#86)

--- keodi-microservice/core-service/src/shared/constants/recommendation.constant.ts
+++ keodi-microservice/core-service/src/shared/constants/recommendation.constant.ts
@@ -2,6 +2,6 @@ export const PLACES_PER_SEARCH_TERM = 2;
 
 export const TIME_DECAY = 0.05;
 
-export const MAX_RECOMMENDATIONS_BOUNDING_KM = 5
+export const MAX_RECOMMENDATIONS_BOUNDING_KM = 5;
 
 export const MAX_CANDINDATE_PLACES = 20;
